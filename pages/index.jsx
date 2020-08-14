ee3979b9c0208f97d549b87adb661bee0e676ae1
beldmian <beldmian@gmail.com>
2020-08-14 21:51:51 +0300

    Raw pagination support

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
 import {useState} from 'react'
 import Guide from '../components/Guide'
 import { Grid, Container, Button, ButtonGroup } from '@material-ui/core'
+import { Pagination } from '@material-ui/lab'
 import Head from 'next/head'
 
 export default function Home({data}) {
   const [filter, setFilter] = useState('all')
+  const [page, setPage] = useState(1)
+  const perPage = 21
   return (
     <Container style={{marginTop: 20}} fixed>
       <Head>
@@ -12,26 +15,24 @@ export default function Home({data}) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <ButtonGroup size="small" variant="outlined" style={{marginBottom: 10}}>
-        <Button onClick={() => { setFilter('all') }} disabled={filter == 'all'}>all</Button>
-        <Button onClick={() => { setFilter('ske') }} disabled={filter == 'ske'}>ske</Button>
-        <Button onClick={() => { setFilter('sco') }} disabled={filter == 'sco'}>sco</Button>
-        <Button onClick={() => { setFilter('mon') }} disabled={filter == 'mon'}>mon</Button>
-        <Button onClick={() => { setFilter('nor') }} disabled={filter == 'nor'}>nor</Button>
-        <Button onClick={() => { setFilter('nil') }} disabled={filter == 'nil'}>nil</Button>
-        <Button onClick={() => { setFilter('syn') }} disabled={filter == 'syn'}>syn</Button>
+        {['all', 'ske', 'sco', 'mon', 'nor', 'nil', 'syn'].map(fract => {
+          return (
+            <Button onClick={() => { setFilter(fract) }} disabled={filter == fract} key={fract}>{fract}</Button>
+          )
+        })}
       </ButtonGroup>
       <Grid container spacing={2}>
-        {filter == 'all' ? data.map(guide => {
+        {filter == 'all' ? data.slice((page-1)*perPage, page*perPage).map(guide => {
           return (
-            <Grid item xs={4} key={guide.id}>
+            <Grid item md={4} sm={6} xs={12} key={guide.id}>
               <Guide
               guide={guide} 
               />
             </Grid>
           )
-        }) : data.filter(guide => guide.faction.short == filter).map(guide => {
+        }) : data.filter(guide => guide.faction.short == filter).slice((page - 1) * perPage, page * perPage).map(guide => {
           return (
-            <Grid item xs={4} key={guide.id}>
+            <Grid item md={4} sm={6} xs={12} key={guide.id}>
               <Guide
                 guide={guide}
               />
@@ -39,6 +40,7 @@ export default function Home({data}) {
           )
         })}
       </Grid>
+      <Pagination style={{margin: '10px 0'}} count={24} page={page} onChange={(e, v) => {setPage(v)}}/>
     </Container>
   )
 }
